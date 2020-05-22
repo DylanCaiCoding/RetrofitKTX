52161080927a8aca5dd62f9b650d1bd24500cd41
dylanc <caishenglang@foxmail.com>
2020-05-22 18:02:37 +0800

    Delete useless comments

--- retrofithelper-rxjava/src/main/java/com/dylanc/retrofit/helper/rxjava/Transformers.kt
+++ retrofithelper-rxjava/src/main/java/com/dylanc/retrofit/helper/rxjava/Transformers.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.dylanc.retrofit.helper.rxjava
 
 import io.reactivex.Flowable
@@ -22,7 +24,6 @@ fun <T> Observable<T>.showLoading(
 fun Observable<ResponseBody>.toFile(pathname: String): Observable<File> =
   compose(Transformers.toFile(pathname))
 
-
 fun <T> Flowable<T>.io2mainThread(): Flowable<T> =
   compose(Transformers.io2mainThread())
 
@@ -32,36 +33,6 @@ fun <T> Flowable<T>.showLoading(
 
 fun Flowable<ResponseBody>.toFile(pathname: String): Flowable<File> =
   compose(Transformers.toFile(pathname))
-
-//fun<T> Flowable<T>.observeDownload(
-//  url: String,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Flowable<T> =
-//  compose(Transformers.observeDownload(url, onProgress, onError))
-//
-//fun<T> Flowable<T>.observeDiffDownloadOnSameUrl(
-//  url: String,
-//  key: String? = null,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Flowable<T> =
-//  compose(Transformers.observeDiffDownloadOnSameUrl(url, key, onProgress, onError))
-//
-//fun <T> Flowable<T>.observeUpload(
-//  url: String,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Flowable<T> =
-//  compose(Transformers.observeUpload(url, onProgress, onError))
-//
-//fun <T> Flowable<T>.observeDiffUploadOnSameUrl(
-//  url: String,
-//  key: String? = null,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Flowable<T> =
-//  compose(Transformers.observeDiffUploadOnSameUrl(url, key, onProgress, onError))
 
 fun <T> Single<T>.io2mainThread(): Single<T> =
   compose(Transformers.io2mainThread())
@@ -73,110 +44,17 @@ fun <T> Single<T>.showLoading(
 fun Single<ResponseBody>.toFile(pathname: String): Single<File> =
   compose(Transformers.toFile(pathname))
 
-//fun<T> Single<T>.observeDownload(
-//  url: String,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Single<T> =
-//  compose(Transformers.observeDownload(url, onProgress, onError))
-//
-//fun <T>Single<T>.observeDiffDownloadOnSameUrl(
-//  url: String,
-//  key: String? = null,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Single<T> =
-//  compose(Transformers.observeDiffDownloadOnSameUrl(url, key, onProgress, onError))
-//
-//fun <T> Single<T>.observeUpload(
-//  url: String,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Single<T> =
-//  compose(Transformers.observeUpload(url, onProgress, onError))
-//
-//fun <T> Single<T>.observeDiffUploadOnSameUrl(
-//  url: String,
-//  key: String? = null,
-//  onProgress: (progressInfo: ProgressInfo) -> Unit,
-//  onError: (id: Long, e: Exception) -> Unit
-//): Single<T> =
-//  compose(Transformers.observeDiffUploadOnSameUrl(url, key, onProgress, onError))
-
 object Transformers {
 
   @JvmStatic
   fun <T> io2mainThread() =
-    ThreadTransformer<T>(
-      Schedulers.io(),
-      AndroidSchedulers.mainThread()
-    )
+    ThreadTransformer<T>(Schedulers.io(), AndroidSchedulers.mainThread())
 
   @JvmStatic
-  fun <T> showLoading(
-    requestLoading: RequestLoading
-  ) = LoadingTransformer<T>(requestLoading)
+  fun <T> showLoading(requestLoading: RequestLoading) =
+    LoadingTransformer<T>(requestLoading)
 
   @JvmStatic
   fun toFile(pathname: String): FileTransformer =
     FileTransformer(pathname)
-
-//  @JvmStatic
-//  fun <T> observeDownload(
-//    url: String,
-//    onProgress: (progressInfo: ProgressInfo) -> Unit,
-//    onError: (id: Long, e: Exception) -> Unit
-//  ) = com.dylanc.retrofit.helper.sample.network.ProgressTransformer<T>(
-//    onProgress,
-//    onError
-//  ) { listener ->
-//    ProgressManager.getInstance().addResponseListener(url, listener)
-//  }
-//
-//  @JvmStatic
-//  fun <T> observeDiffDownloadOnSameUrl(
-//    url: String,
-//    key: String? = null,
-//    onProgress: (progressInfo: ProgressInfo) -> Unit,
-//    onError: (id: Long, e: Exception) -> Unit
-//  ) = com.dylanc.retrofit.helper.sample.network.ProgressTransformer<T>(
-//    onProgress,
-//    onError
-//  ) { listener ->
-//    if (key == null) {
-//      ProgressManager.getInstance().addDiffResponseListenerOnSameUrl(url, listener)
-//    } else {
-//      ProgressManager.getInstance().addDiffResponseListenerOnSameUrl(url, key, listener)
-//    }
-//  }
-//
-//  @JvmStatic
-//  fun <T> observeUpload(
-//    url: String,
-//    onProgress: (progressInfo: ProgressInfo) -> Unit,
-//    onError: (id: Long, e: Exception) -> Unit
-//  ) = com.dylanc.retrofit.helper.sample.network.ProgressTransformer<T>(
-//    onProgress,
-//    onError
-//  ) { listener ->
-//    ProgressManager.getInstance().addRequestListener(url, listener)
-//  }
-//
-//  @JvmStatic
-//  fun <T> observeDiffUploadOnSameUrl(
-//    url: String,
-//    key: String? = null,
-//    onProgress: (progressInfo: ProgressInfo) -> Unit,
-//    onError: (id: Long, e: Exception) -> Unit
-//  ) = com.dylanc.retrofit.helper.sample.network.ProgressTransformer<T>(
-//    onProgress,
-//    onError
-//  ) { listener ->
-//    if (key == null) {
-//      ProgressManager.getInstance().addDiffRequestListenerOnSameUrl(url, listener)
-//    } else {
-//      ProgressManager.getInstance().addDiffRequestListenerOnSameUrl(url, key, listener)
-//    }
-//  }
-
 }
