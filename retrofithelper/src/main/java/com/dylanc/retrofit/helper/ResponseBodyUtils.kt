9cf4fbf614f0136d2b3b1156700d3164dec0d6b2
caisl <caishenglang@foxmail.com>
2020-06-22 00:23:19 +0800

    Add ResponseBodyUtils.kt

---
+++ retrofithelper/src/main/java/com/dylanc/retrofit/helper/ResponseBodyUtils.kt
@@ -0,0 +1,18 @@
+@file:JvmName("ResponseBodyUtils")
+
+package com.dylanc.retrofit.helper
+
+import okhttp3.ResponseBody
+import java.io.File
+
+/**
+ * @author Dylan Cai
+ */
+fun ResponseBody.toFile(pathname: String): File =
+  File(pathname).apply {
+    use {
+      outputStream().use { fileOut ->
+        byteStream().copyTo(fileOut)
+      }
+    }
+  }
