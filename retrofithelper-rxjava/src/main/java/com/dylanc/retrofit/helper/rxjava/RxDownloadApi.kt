e8c41b9d0a623a624c7e026eb8c7d13a48d2dc90
caisl <caishenglang@foxmail.com>
2020-12-01 01:07:18 +0800

    Add RxDownloadApi

---
+++ retrofithelper-rxjava/src/main/java/com/dylanc/retrofit/helper/rxjava/RxDownloadApi.kt
@@ -0,0 +1,41 @@
+@file:Suppress("unused")
+
+package com.dylanc.retrofit.helper.rxjava
+
+import io.reactivex.Single
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Streaming
+import retrofit2.http.Url
+
+/**
+ * @author Dylan Cai
+ */
+
+fun RxDownloadApi.download(url: String, startByte: Int) =
+  download(url, "bytes=$startByte-")
+
+fun RxDownloadApi.download(url: String, vararg bytesRange: Pair<Int, Int?>): Single<ResponseBody> {
+  val stringBuilder = StringBuilder()
+  for ((start, end) in bytesRange) {
+    if (stringBuilder.isBlank()) {
+      stringBuilder.append("bytes=$start-${end ?: ""}")
+    } else {
+      stringBuilder.append(",$start-${end ?: ""}")
+    }
+  }
+  return download(url, stringBuilder.toString())
+}
+
+interface RxDownloadApi {
+
+  @Streaming
+  @GET
+  fun download(@Url url: String): Single<ResponseBody>
+
+
+  @Streaming
+  @GET
+  fun download(@Url url: String, @Header("Range") range: String): Single<ResponseBody>
+}
