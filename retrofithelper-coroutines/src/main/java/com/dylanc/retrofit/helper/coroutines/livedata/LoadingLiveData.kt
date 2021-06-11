c3ca00265a3162b2dce69ccc63d8e037d2ad251e
Dylanc <caishenglang@foxmail.com>
2021-06-11 19:55:04 +0800

    Optimize LoadingLiveData

--- retrofithelper-coroutines/src/main/java/com/dylanc/retrofit/helper/coroutines/livedata/LoadingLiveData.kt
+++ retrofithelper-coroutines/src/main/java/com/dylanc/retrofit/helper/coroutines/livedata/LoadingLiveData.kt
@@ -6,6 +6,7 @@ import android.app.Dialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onCompletion
@@ -29,10 +30,10 @@ inline fun LoadingLiveData.observe(
   dialog: DialogFragment,
   tag: String = TAG_LOADING
 ) {
-  observe(activity) {
-    if (it) {
+  observe(activity) { show ->
+    if (show && dialog.dialog?.isShowing != true && dialog.isRemoving) {
       dialog.show(activity.supportFragmentManager, tag)
-    } else if (dialog.dialog != null) {
+    } else if (!show && dialog.dialog?.isShowing == true && !dialog.isRemoving) {
       dialog.dismiss()
     }
   }
@@ -43,23 +44,21 @@ inline fun LoadingLiveData.observe(
   dialog: DialogFragment,
   tag: String = TAG_LOADING
 ) {
-  observe(fragment.viewLifecycleOwner) {
-    if (it) {
+  observe(fragment.viewLifecycleOwner) { isLoading ->
+    if (isLoading && dialog.dialog?.isShowing != true && dialog.isRemoving) {
       dialog.show(fragment.parentFragmentManager, tag)
-    } else if (dialog.dialog != null) {
+    } else if (!isLoading && dialog.dialog?.isShowing == true && !dialog.isRemoving) {
       dialog.dismiss()
     }
   }
 }
 
-inline fun LoadingLiveData.observe(activity: FragmentActivity, dialog: Dialog?) {
-  observe(activity) { isLoading ->
-    dialog?.let { if (isLoading) it.show() else it.dismiss() }
-  }
-}
-
-inline fun LoadingLiveData.observe(fragment: Fragment, dialog: Dialog?) {
-  observe(fragment.viewLifecycleOwner) { isLoading ->
-    dialog?.let { if (isLoading) it.show() else it.dismiss() }
+inline fun LoadingLiveData.observe(lifecycleOwner: LifecycleOwner, dialog: Dialog?) {
+  observe(lifecycleOwner) { isLoading ->
+    if (isLoading && dialog?.isShowing != true) {
+      dialog?.show()
+    } else if (!isLoading && dialog?.isShowing == true) {
+      dialog.dismiss()
+    }
   }
 }
